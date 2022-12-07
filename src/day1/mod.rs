148eceaa3c4d544bfd478aaaa1dde04d0538406f
Grendel <jcg@grendel.world>
2022-12-07 14:43:03 -0600

    Remove use statement for mods used only once

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,12 +1,10 @@
-use crate::slurp;
-
 pub fn execute() {
     crate::headline::print_day(1);
 
     let mut sum = 0u64;
     let mut sums: Vec<u64> = Vec::new();
 
-    let lines = slurp::read_lines("src/day1/input.txt").expect("Couldn't get day 1 input");
+    let lines = crate::slurp::read_lines("src/day1/input.txt").expect("Couldn't get day 1 input");
 
     for line in lines {
         let text = line.expect("Not a valid string");
--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,10 +1,7 @@
-use crate::slurp;
-
 pub fn execute() {
     crate::headline::print_day(2);
 
-    let lines = slurp::read_lines("src/day2/input.txt")
-        .expect("Couldn't get day 2 input");
+    let lines = crate::slurp::read_lines("src/day2/input.txt").expect("Couldn't get day 2 input");
 
     for line in lines {
         let _text = line.expect("Not a valid string");
