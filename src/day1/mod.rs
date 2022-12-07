c4f99e82b5d0fb40207b5bc290b0de85b91e4551
Grendel <jcg@grendel.world>
2022-12-07 14:49:50 -0600

    Move inputs out of src

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -4,7 +4,7 @@ pub fn execute() {
     let mut sum = 0u64;
     let mut sums: Vec<u64> = Vec::new();
 
-    let lines = crate::slurp::read_lines("src/day1/input.txt").expect("Couldn't get day 1 input");
+    let lines = crate::slurp::read_lines("inputs/1.txt").expect("Couldn't get day 1 input");
 
     for line in lines {
         let text = line.expect("Not a valid string");
--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,7 +1,7 @@
 pub fn execute() {
     crate::headline::print_day(2);
 
-    let lines = crate::slurp::read_lines("src/day2/input.txt").expect("Couldn't get day 2 input");
+    let lines = crate::slurp::read_lines("inputs/2.txt").expect("Couldn't get day 2 input");
 
     for line in lines {
         let _text = line.expect("Not a valid string");
