01fe8cd273590498f3735b602389fa6b03623299
Grendel <jcg@grendel.world>
2022-12-07 21:30:01 -0600

    Add day 4 part 1

---
+++ src/day4.rs
@@ -0,0 +1,25 @@
+pub fn execute() {
+    let inputs = crate::start_day::setup(4);
+
+    let mut overlaps = 0usize;
+
+    for line in inputs {
+        let (a, b) = line.split_once(',').expect("Malformed input");
+
+        let a = match a.split_once('-') {
+            Some((x, y)) => (x.parse::<u32>().unwrap(), y.parse::<u32>().unwrap()),
+            None => panic!("Malformed range"),
+        };
+
+        let b = match b.split_once('-') {
+            Some((x, y)) => (x.parse::<u32>().unwrap(), y.parse::<u32>().unwrap()),
+            None => panic!("Malformed range"),
+        };
+
+        if a.0 <= b.0 && a.1 >= b.1 || b.0 <= a.0 && b.1 >= a.1 {
+            overlaps += 1;
+        }
+    }
+
+    println!("Part 1: {}", overlaps);
+}
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ mod start_day;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     println!("Advent of Code 2022");
     day1::execute();
     day2::execute();
     day3::execute();
+    day4::execute();
 }
