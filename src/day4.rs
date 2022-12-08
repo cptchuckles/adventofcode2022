f268eaa02688d50b57dfb411191da4138fcafdc3
Grendel <jcg@grendel.world>
2022-12-07 21:36:27 -0600

    Finish day 4 part 2

--- src/day4.rs
+++ src/day4.rs
@@ -2,6 +2,7 @@ pub fn execute() {
     let inputs = crate::start_day::setup(4);
 
     let mut overlaps = 0usize;
+    let mut encapsulations = 0usize;
 
     for line in inputs {
         let (a, b) = line.split_once(',').expect("Malformed input");
@@ -17,9 +18,18 @@ pub fn execute() {
         };
 
         if a.0 <= b.0 && a.1 >= b.1 || b.0 <= a.0 && b.1 >= a.1 {
+            encapsulations += 1;
+        }
+
+        if b.0 <= a.0 && a.0 <= b.1
+            || b.0 <= a.1 && a.1 <= b.1
+            || a.0 <= b.0 && b.0 <= a.1
+            || a.0 <= b.1 && b.1 <= a.1
+        {
             overlaps += 1;
         }
     }
 
-    println!("Part 1: {}", overlaps);
+    println!("Part 1: {}", encapsulations);
+    println!("Part 2: {}", overlaps);
 }
