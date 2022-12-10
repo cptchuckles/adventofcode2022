49e3417581faed3a4afee427ecc257aa946e22c9
Grendel <jcg@grendel.world>
2022-12-10 13:39:23 -0600

    Finish day 7 part 2

--- src/day7.rs
+++ src/day7.rs
@@ -31,11 +31,20 @@ pub fn execute() {
 
     let mut total_sizes = 0u64;
 
+    let du = *dirs.get("/").unwrap();
+    let space_available = 70_000_000u64 - du;
+    let space_needed = 30_000_000u64 - space_available;
+    let mut smallest_nuked_dir_size = du;
+
     for (_, v) in dirs {
         if v <= 100000 {
             total_sizes += v;
         }
+        if v >= space_needed {
+            smallest_nuked_dir_size = std::cmp::min(smallest_nuked_dir_size, v);
+        }
     }
 
     println!("Part 1: {}", total_sizes);
+    println!("Part 2: {}", smallest_nuked_dir_size);
 }
