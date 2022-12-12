5c07d52b31a465f504f2394dcfb7ea53cc6817f4
Grendel <jcg@grendel.world>
2022-12-11 18:37:50 -0600

    Use HashSet to count visible trees

--- src/day8.rs
+++ src/day8.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 pub fn execute() {
     let inputs = crate::start_day::setup(8);
@@ -7,38 +7,39 @@ pub fn execute() {
         .map(|s| s.chars().map(|c| (c as u8) - b'0').collect())
         .collect::<Vec<Vec<u8>>>();
 
+    println!("Part 1: {}", count_visible_trees(&grid));
+}
+
+fn count_visible_trees(grid: &Vec<Vec<u8>>) -> usize {
     let (rows, cols) = (grid.len(), grid[0].len());
-    let mut visited_trees: HashMap<(usize, usize), bool> = HashMap::new();
+    let mut visible_trees: HashSet<(usize, usize)> = HashSet::new();
 
     for row in 0..rows {
-        visited_trees.insert((row, 0), true);
-        visited_trees.insert((row, cols - 1), true);
+        visible_trees.insert((row, 0));
+        visible_trees.insert((row, cols - 1));
     }
 
     for col in 1..(cols - 1) {
-        visited_trees.insert((0, col), true);
-        visited_trees.insert((rows - 1, col), true);
+        visible_trees.insert((0, col));
+        visible_trees.insert((rows - 1, col));
     }
 
-    {
-        let visits = &mut visited_trees;
-        // From the north
-        search_vertical(&grid, visits, 0..rows, 0..cols);
-        // From the south
-        search_vertical(&grid, visits, (0..rows).rev(), 0..cols);
-        // From the west
-        search_horizontal(&grid, visits, 0..rows, 0..cols);
-        // From the east
-        search_horizontal(&grid, visits, 0..rows, (0..cols).rev());
-    }
+    let visits = &mut visible_trees;
+    // From the north
+    search_vertical(grid, visits, 0..rows, 0..cols);
+    // From the south
+    search_vertical(grid, visits, (0..rows).rev(), 0..cols);
+    // From the west
+    search_horizontal(grid, visits, 0..rows, 0..cols);
+    // From the east
+    search_horizontal(grid, visits, 0..rows, (0..cols).rev());
 
-    let visible_trees = visited_trees.values().filter(|v| **v).count();
-    println!("Part 1: {}", visible_trees);
+    visible_trees.iter().count()
 }
 
 fn search_vertical<I1, I2>(
     grid: &Vec<Vec<u8>>,
-    visited_trees: &mut HashMap<(usize, usize), bool>,
+    visible_trees: &mut HashSet<(usize, usize)>,
     rows: I1,
     cols: I2,
 ) where
@@ -48,16 +49,10 @@ fn search_vertical<I1, I2>(
     for col in cols {
         let mut highest = 0;
         for row in rows.clone() {
-            let mut visible = false;
             let height = grid[row][col];
             if grid[row][col] > highest {
-                visible = true;
                 highest = height;
-            }
-            if let Some(visibility) = visited_trees.insert((row, col), visible) {
-                if visibility != visible {
-                    visited_trees.insert((row, col), true);
-                }
+                visible_trees.insert((row, col));
             }
         }
     }
@@ -65,7 +60,7 @@ fn search_vertical<I1, I2>(
 
 fn search_horizontal<I1, I2>(
     grid: &Vec<Vec<u8>>,
-    visited_trees: &mut HashMap<(usize, usize), bool>,
+    visible_trees: &mut HashSet<(usize, usize)>,
     rows: I1,
     cols: I2,
 ) where
@@ -75,16 +70,10 @@ fn search_horizontal<I1, I2>(
     for row in rows {
         let mut highest = 0;
         for col in cols.clone() {
-            let mut visible = false;
             let height = grid[row][col];
             if grid[row][col] > highest {
-                visible = true;
                 highest = height;
-            }
-            if let Some(visibility) = visited_trees.insert((row, col), visible) {
-                if visibility != visible {
-                    visited_trees.insert((row, col), true);
-                }
+                visible_trees.insert((row, col));
             }
         }
     }
