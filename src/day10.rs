8576ccd8a1b9967e758ad12522e12a50d2248363
Grendel <jcg@grendel.world>
2022-12-14 15:57:07 -0600

    Finish day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -5,17 +5,18 @@ pub fn execute() {
     let mut x = 1i64;
 
     let mut sum = 0i64;
+    let mut crt: [char; 240] = ['.'; 240];
 
     for input in inputs {
         match input.as_str() {
             "noop" => {
-                if tick(&mut clock) {
+                if tick(&mut clock, &mut crt, x) {
                     sum += clock * x;
                 }
             },
             addx => {
                 for _ in 0..2 {
-                    if tick(&mut clock) {
+                    if tick(&mut clock, &mut crt, x) {
                         sum += clock * x;
                     }
                 }
@@ -25,9 +26,27 @@ pub fn execute() {
     }
 
     println!("Part 1: {}", sum);
+
+    print!("Part 2:");
+    print_crt(&crt);
 }
 
-fn tick(clock: &mut i64) -> bool {
+fn print_crt(crt: &[char]) {
+    for (i, c) in crt.iter().enumerate() {
+        if i % 40 == 0 {
+            println!();
+        }
+        print!("{}", c);
+    }
+    println!();
+}
+
+fn tick(clock: &mut i64, crt: &mut [char], x: i64) -> bool {
+    let row = *clock / 40;
+    let col = *clock - 40 * row;
+    if (x - 1) <= col && col <= (x + 1) {
+        crt[*clock as usize] = '#';
+    }
     *clock += 1;
     (*clock + 20) % 40 == 0
 }
