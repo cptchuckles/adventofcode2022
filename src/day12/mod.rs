66a392a9067865b78996183cd766397baddb3041
Grendel <jcg@grendel.world>
2022-12-20 00:18:14 -0600

    Make fringe Vec into a HashMap for blazingly fastness

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -5,14 +5,17 @@ use std::collections::HashMap;
 pub fn execute() {
     let terrain = Terrain::from_input(crate::start_day::setup("12"));
 
-    let mut fringe: Vec<Node> = Vec::new();
+    let mut fringe: HashMap<(usize, usize), Node> = HashMap::new();
     let mut closed: HashMap<(usize, usize), Node> = HashMap::new();
     let mut path_length: u32 = u32::MAX;
     let mut paths: Vec<Vec<(usize, usize)>> = Vec::new();
 
-    fringe.push(terrain.make_node(terrain.start, None, 0));
+    fringe.insert(terrain.start, terrain.make_node(terrain.start, None, 0));
 
-    while let Some(current) = fringe.pop() {
+    while fringe.len() > 0 {
+        let current = *fringe.values().min_by_key(|n| n.get_f()).unwrap();
+        fringe.remove(&current.pos);
+
         if current.pos == terrain.goal {
             path_length = std::cmp::min(path_length, current.g);
             let mut path = vec![current.pos];
@@ -25,25 +28,19 @@ pub fn execute() {
             continue;
         }
 
-        'neighbors: for neighbor in terrain.get_neighbors(current) {
+        for neighbor in terrain.get_neighbors(current) {
             if closed.contains_key(&neighbor.pos) {
                 continue;
             }
-            for (i, node) in fringe.iter().enumerate() {
-                if node.pos == neighbor.pos {
-                    if node.g < neighbor.g {
-                        continue 'neighbors;
-                    }
-                    fringe[i] = neighbor;
-                    continue 'neighbors;
+            if let Some(node) = fringe.get(&neighbor.pos) {
+                if node.g < neighbor.g {
+                    continue;
                 }
             }
-            fringe.push(neighbor);
+            fringe.insert(neighbor.pos, neighbor);
         }
 
         closed.insert(current.pos, current);
-
-        fringe.sort_by(|a, b| b.get_f().cmp(&a.get_f()));
     }
 
     println!("Part 1: {}", path_length);
