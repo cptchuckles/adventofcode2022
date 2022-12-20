6d0b8f35df895ca1910e81ecfdaea17efdd6d2dc
Grendel <jcg@grendel.world>
2022-12-20 00:25:20 -0600

    Revert "Make fringe Vec into a HashMap for blazingly fastness"
    This reverts commit 66a392a9067865b78996183cd766397baddb3041.

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -5,17 +5,14 @@ use std::collections::HashMap;
 pub fn execute() {
     let terrain = Terrain::from_input(crate::start_day::setup("12"));
 
-    let mut fringe: HashMap<(usize, usize), Node> = HashMap::new();
+    let mut fringe: Vec<Node> = Vec::new();
     let mut closed: HashMap<(usize, usize), Node> = HashMap::new();
     let mut path_length: u32 = u32::MAX;
     let mut paths: Vec<Vec<(usize, usize)>> = Vec::new();
 
-    fringe.insert(terrain.start, terrain.make_node(terrain.start, None, 0));
+    fringe.push(terrain.make_node(terrain.start, None, 0));
 
-    while fringe.len() > 0 {
-        let current = *fringe.values().min_by_key(|n| n.get_f()).unwrap();
-        fringe.remove(&current.pos);
-
+    while let Some(current) = fringe.pop() {
         if current.pos == terrain.goal {
             path_length = std::cmp::min(path_length, current.g);
             let mut path = vec![current.pos];
@@ -28,19 +25,25 @@ pub fn execute() {
             continue;
         }
 
-        for neighbor in terrain.get_neighbors(current) {
+        'neighbors: for neighbor in terrain.get_neighbors(current) {
             if closed.contains_key(&neighbor.pos) {
                 continue;
             }
-            if let Some(node) = fringe.get(&neighbor.pos) {
-                if node.g < neighbor.g {
-                    continue;
+            for (i, node) in fringe.iter().enumerate() {
+                if node.pos == neighbor.pos {
+                    if node.g < neighbor.g {
+                        continue 'neighbors;
+                    }
+                    fringe[i] = neighbor;
+                    continue 'neighbors;
                 }
             }
-            fringe.insert(neighbor.pos, neighbor);
+            fringe.push(neighbor);
         }
 
         closed.insert(current.pos, current);
+
+        fringe.sort_by(|a, b| b.get_f().cmp(&a.get_f()));
     }
 
     println!("Part 1: {}", path_length);
